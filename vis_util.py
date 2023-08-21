0a9ea440a056550719132e3508cc36935c145cc3
fengyu.jzy <fengyu.jzy@alibaba-inc.com>
2023-08-21 18:58:26 +0800

    add create_class_visualization for vis_util

--- vis_util.py
+++ vis_util.py
@@ -1,6 +1,10 @@
+import random
 import numpy as np
 import torch
 import torchvision
+import torchvision.transforms as T
+from scipy.ndimage.filters import gaussian_filter1d
+import matplotlib.pyplot as plt
 
 
 def plot_latent_space(model, latent_dim, latent_index=0, range=(-2.5, 2.5), nrow=10):
@@ -28,3 +32,144 @@ def plot_latent_space(model, latent_dim, latent_index=0, range=(-2.5, 2.5), nrow
     # show_image(torchvision.utils.make_grid(image_recon.data[:100],10,1), ax, "")
     # plt.savefig('sample.pdf', dpi=300, format='pdf')
     # plt.show()
+
+
+##############################
+# from cs231n
+# https://github.com/jariasf/CS231n/blob/master/assignment3/NetworkVisualization-PyTorch.ipynb
+##############################
+SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+SQUEEZENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+
+
+def preprocess(img, size=224):
+    transform = T.Compose([
+        T.Resize(size),
+        T.ToTensor(),
+        T.Normalize(mean=SQUEEZENET_MEAN.tolist(),
+                    std=SQUEEZENET_STD.tolist()),
+        T.Lambda(lambda x: x[None]),
+    ])
+    return transform(img)
+
+
+def deprocess(img, should_rescale=True):
+    transform = T.Compose([
+        T.Lambda(lambda x: x[0]),
+        T.Normalize(mean=[0, 0, 0], std=(1.0 / SQUEEZENET_STD).tolist()),
+        T.Normalize(mean=(-SQUEEZENET_MEAN).tolist(), std=[1, 1, 1]),
+        T.Lambda(rescale) if should_rescale else T.Lambda(lambda x: x),
+        T.ToPILImage(),
+    ])
+    return transform(img)
+
+
+def rescale(x):
+    low, high = x.min(), x.max()
+    x_rescaled = (x - low) / (high - low)
+    return x_rescaled
+
+
+def blur_image(X, sigma=1):
+    X_np = X.cpu().clone().numpy()
+    X_np = gaussian_filter1d(X_np, sigma, axis=2)
+    X_np = gaussian_filter1d(X_np, sigma, axis=3)
+    X.copy_(torch.Tensor(X_np).type_as(X))
+    return X
+
+
+def jitter(X, ox, oy):
+    """
+    Helper function to randomly jitter an image.
+
+    Inputs
+    - X: PyTorch Tensor of shape (N, C, H, W)
+    - ox, oy: Integers giving number of pixels to jitter along W and H axes
+
+    Returns: A new PyTorch Tensor of shape (N, C, H, W)
+    """
+    if ox != 0:
+        left = X[:, :, :, :-ox]
+        right = X[:, :, :, -ox:]
+        X = torch.cat([right, left], dim=3)
+    if oy != 0:
+        top = X[:, :, :-oy]
+        bottom = X[:, :, -oy:]
+        X = torch.cat([bottom, top], dim=2)
+    return X
+
+
+def create_class_visualization(model, target_value, img_shape, dtype, **kwargs):
+    """
+    Generate an image to maximize the score of target_y under a pretrained model.
+
+    Inputs:
+    - img_shape: (C, H, W)
+    - model: A pretrained CNN that will be used to generate the image
+    - dtype: Torch datatype to use for computations
+
+    Keyword arguments:
+    - l2_reg: Strength of L2 regularization on the image
+    - learning_rate: How big of a step to take
+    - num_iterations: How many iterations to use
+    - blur_every: How often to blur the image as an implicit regularizer
+    - max_jitter: How much to gjitter the image as an implicit regularizer
+    - show_every: How often to show the intermediate result
+    """
+    model.type(dtype)
+    l2_reg = kwargs.pop('l2_reg', 1e-3)
+    learning_rate = kwargs.pop('learning_rate', 25)
+    num_iterations = kwargs.pop('num_iterations', 100)
+    blur_every = kwargs.pop('blur_every', 10)
+    max_jitter = kwargs.pop('max_jitter', 16)
+    show_every = kwargs.pop('show_every', 25)
+
+    # Randomly initialize the image as a PyTorch Tensor, and make it requires gradient.
+    img = torch.randn(1, img_shape[0], img_shape[1], img_shape[2]).mul_(1.0).type(dtype).requires_grad_()
+
+    for t in range(num_iterations):
+        # Randomly jitter the image a bit; this gives slightly nicer results
+        ox, oy = random.randint(0, max_jitter), random.randint(0, max_jitter)
+        img.data.copy_(jitter(img.data, ox, oy))
+
+        ########################################################################
+        # TODO: Use the model to compute the gradient of the score for the     #
+        # class target_y with respect to the pixels of the image, and make a   #
+        # gradient step on the image using the learning rate. Don't forget the #
+        # L2 regularization term!                                              #
+        # Be very careful about the signs of elements in your code.            #
+        ########################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        dist, value, x_recon, latent_mu, latent_logvar = model(rescale(img))
+        loss = 0.5 * (value - target_value).pow(2) - l2_reg * torch.norm(img)
+        loss.backward()
+        with torch.no_grad():
+            img += learning_rate * img.grad / torch.norm(img.grad)
+            img.grad.zero_()
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ########################################################################
+        #                             END OF YOUR CODE                         #
+        ########################################################################
+
+        # Undo the random jitter
+        img.data.copy_(jitter(img.data, -ox, -oy))
+
+        # As regularizer, clamp and periodically blur the image
+        for c in range(img_shape[0]):
+            lo = float(-SQUEEZENET_MEAN[c] / SQUEEZENET_STD[c])
+            hi = float((1.0 - SQUEEZENET_MEAN[c]) / SQUEEZENET_STD[c])
+            img.data[:, c].clamp_(min=lo, max=hi)
+        if t % blur_every == 0:
+            blur_image(img.data, sigma=0.5)
+
+        # Periodically show the image
+        if t == 0 or (t + 1) % show_every == 0 or t == num_iterations - 1:
+            plt.imshow(rescale(img.data.clone().cpu()[0].permute(1,2,0)), cmap='gray')
+            plt.title('Iteration %d / %d' % (t + 1, num_iterations))
+            plt.gcf().set_size_inches(4, 4)
+            plt.axis('off')
+            plt.show()
+
+    return img.data.cpu()
